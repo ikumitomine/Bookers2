79fd8fb0fdf328523051b474139dc7c3e7c60d74
東峰いくみ <ikumitomine@touhouiuminombp.lan>
2020-08-30 19:54:57 +0900

    [Add]投稿/閲覧機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :users, only:[:show, :index, :edit, :update]
+  # get 'home/about'
+  root to: 'home#top'
+  resources :books
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,8 +9,16 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+rails s -b 0.0.0.0
+ActiveRecord::Schema.define(version: 2020_08_29_015546) do
 
-ActiveRecord::Schema.define(version: 2020_08_28_072522) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
